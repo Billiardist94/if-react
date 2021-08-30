f06c97ef60eb0751977d000d9decf208060cbaa0
Pavel Krasnov <https://github.com/Billiardist94/if-html.git>
2021-08-30 18:25:02 +0300

    lesson-39. SignIn component.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import { NavLink } from 'react-router-dom';
+import SignOut from '../SignIn_SignOut/SignOut';
 
 const Header = () => {
+  const [isClicked, setClicked] = useState(false);
+
+  const isSignedIn = localStorage.getItem('isSignedIn');
+
+  const handleClick = () => {
+    setClicked(!isClicked);
+  };
+
   return (
     <header className="header">
       <div className="container">
@@ -48,18 +58,21 @@ const Header = () => {
                 </svg>
               </li>
               <li className="header-item">
-                <svg
-                  width="40"
-                  height="40"
-                  viewBox="0 0 40 40"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M20 0C8.96 0 0 8.96 0 20s8.96 20 20 20 20-8.96 20-20S31.04 0 20 0zm0 6c3.32 0 6 2.68 6 6s-2.68 6-6 6-6-2.68-6-6 2.68-6 6-6zm0 28.4c-5 0-9.42-2.56-12-6.44.06-3.98 8-6.16 12-6.16 3.98 0 11.94 2.18 12 6.16-2.58 3.88-7 6.44-12 6.44z"
-                    fill="#fff"
-                  />
-                </svg>
+                <NavLink to="/signin" onClick={ isSignedIn === false || isSignedIn === null ? () => {} : handleClick}>
+                  <svg
+                    width="40"
+                    height="40"
+                    viewBox="0 0 40 40"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M20 0C8.96 0 0 8.96 0 20s8.96 20 20 20 20-8.96 20-20S31.04 0 20 0zm0 6c3.32 0 6 2.68 6 6s-2.68 6-6 6-6-2.68-6-6 2.68-6 6-6zm0 28.4c-5 0-9.42-2.56-12-6.44.06-3.98 8-6.16 12-6.16 3.98 0 11.94 2.18 12 6.16-2.58 3.88-7 6.44-12 6.44z"
+                      fill="#fff"
+                    />
+                  </svg>
+                </NavLink>
+                <SignOut isClicked={isClicked} />
               </li>
             </ul>
             <div className="header-menu">
