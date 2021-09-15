4d0ea8ec8e3d7eddba35018d96f1b38701d10863
Pavel Krasnov <https://github.com/Billiardist94/if-html.git>
2021-09-15 18:14:39 +0300

    lesson-41. Using redux-action and redux-persist.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import reportWebVitals from './reportWebVitals';
-import store from './store/index';
 import App from './components/App';
+import { store, persistor } from './store/index';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
